f92b12795ec43fbd78fb7823457cd5486154eafa
mAzmeerm <azmeerzuli22@gmail.com>
2026-05-11 11:51:28 +0800

    conect ke database

---
+++ dbconn.php
@@ -0,0 +1,15 @@
+
+<?php
+/* php& mysqldb connection file */
+$user = "root"; //mysqlusername
+$pass = ""; //mysqlpassword
+$host = "localhost"; //server name or ipaddress
+$dbname= "expensemanagementsystem"; //your db name
+
+/*echo $user ."<br>";
+echo $pass ."<br>";
+echo $host ."<br>";
+echo $dbname ."<br>"; */
+
+$dbconn= mysqli_connect($host, $user, $pass,$dbname) or die(mysqli_error($dbconn));
+?>
