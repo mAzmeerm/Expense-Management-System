4f605d1d8f78f8f5958d1df16bdbeb0a3abb24a1
mAzmeerm <azmeerzuli22@gmail.com>
2026-05-15 18:25:40 +0800

    basic login

--- login.php
+++ login.php
@@ -1,4 +1,17 @@
 <html>
 <head>
-    <title>Login</title>
+    <title>Login Page</title>
+        <form action="login_process.php" method="post">
+            <input type =" radio" name="role" value="Employee">Employee
+            <input type="radio" name="role" value="Admin">Admin
+            <br>
+            <label for="Email">Username:</label>
+            <input type="text" id="Email" name="Email" required>
+            <br>
+            <label for="Password">Password:</label>
+            <input type="password" id="Password" name="Password" required>
+            <br>
+            <input type="submit" value="Login">
+        </form>
+    </body>
 </html>
