9bd77c636624fd549e109f65eff3c7f8f2ce914b
NeNaSzz <mna.nnz06@gmail.com>
2026-05-19 21:53:31 +0800

    employeeDash

--- function.php
+++ function.php
@@ -1,7 +1,18 @@
 <?php
 include("dbconn.php");
+
 function money($amount) {
     return 'RM ' . number_format((float)$amount, 2);
+}
+
+function e($value)
+{
+    return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+}
+
+if (!isset($_SESSION['UserID']) || $_SESSION['UserID'] === '') {
+    header("Location: login.php");
+    exit();
 }
 
 ?>
